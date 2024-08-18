e736e21c423bc14129a2f9c32742994d2f43278b
Kyle <kylemorris8484@gmail.com>
2024-08-18 22:40:59 +0100

    Initial mui testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import * as React from 'react';
+import { AppBar, Toolbar, IconButton, Drawer } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
+  const [sideDraw, setSideDraw] = React.useState(false);
+  function toggleSideDraw() {
+    setSideDraw(oldState => !oldState)
+  }
+  
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <AppBar position="sticky" sx={{
+        'background-color': 'green',
+        'font-family' : 'roboto',
+        'font-weight' : 500,
+        'font-size' : 25
+      }}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+            onClick={toggleSideDraw}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Drawer
+            open={sideDraw}
+            onClose={toggleSideDraw}
+          >
+            Test
+          </Drawer>
+          Run Planner
+        </Toolbar>
+      </AppBar>
     </>
   )
 }
-
-export default App
