b1fff27bc041a33c4bf7386b8538a4a9beb2bb59
Kyle <kylemorris8484@gmail.com>
2024-09-18 08:12:30 +0100

    Flesh out homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,62 @@
 import * as React from 'react';
-import { AppBar, Toolbar, IconButton, Drawer } from '@mui/material';
-import MenuIcon from '@mui/icons-material/Menu';
+import { AppBar, Toolbar, Typography, Box, Button, } from '@mui/material';
+import { styled } from '@mui/system'
+import { ThemeProvider, useTheme } from '@mui/material/styles';
+import DirectionsRunIcon from '@mui/icons-material/DirectionsRun';
+// import hills from './assets/hillsDuplicated.png'
+// import clouds from './assets/clouds.png'
+import "./App.css"
+import BasicMenu from './BasicMenu.jsx'
 
 export default function App() {
 
-  const [sideDraw, setSideDraw] = React.useState(false);
-  function toggleSideDraw() {
-    setSideDraw(oldState => !oldState)
-  }
-  
+  const theme = useTheme()
+
+  const HomePageButton = styled(Button)(({ theme }) => ({
+    width: "20%",
+    padding: "2%",
+    borderRadius: 7
+  }))
+
+  const Banner = styled(AppBar)(({theme}) => ({
+    position: "sticky",
+    display: "flex",
+    flexDirection: "row",
+    alignItems : "center",
+    height: "10%"
+  }))
+
   return (
-    <>
-      <AppBar position="sticky" sx={{
-        'background-color': 'green',
-        'font-family' : 'roboto',
-        'font-weight' : 500,
-        'font-size' : 25
-      }}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2 }}
-            onClick={toggleSideDraw}
-          >
-            <MenuIcon />
-          </IconButton>
-          <Drawer
-            open={sideDraw}
-            onClose={toggleSideDraw}
-          >
-            Test
-          </Drawer>
-          Run Planner
-        </Toolbar>
-      </AppBar>
-    </>
+    <ThemeProvider theme={theme}>
+      <div className="page" style={{height: screen.height}}>
+
+        <Banner>
+          <DirectionsRunIcon sx={{ width: theme.spacing(3.5), margin: theme.spacing(2) }}/>
+          <Toolbar style={{ padding: 0 }}>
+            <Typography variant='h2'>Run Planner</Typography>
+          </Toolbar>
+          <BasicMenu sx={{ marginLeft:"auto", marginRight: 2 }}/>
+        </Banner> 
+
+        <Box sx={{ "mt" : 4 }}>
+          <Typography align="center">
+            A route planning app to measure distance, altitude change and calories burned for your runs :)
+          </Typography>
+          <Box sx={{display: "flex", justifyContent: "space-evenly", mt: 4}}>
+            <HomePageButton variant="contained" color="secondary">
+              <Typography variant="h4" sx={{fontWeight: 300}}>New route</Typography>
+            </HomePageButton>
+            <HomePageButton variant="contained" color="secondary">
+              <Typography variant="h4" sx={{fontWeight: 300}}>My routes</Typography>
+            </HomePageButton>
+          </Box>
+        </Box>
+
+        {/* <img className="clouds" src={clouds}></img>
+        <img className="hills" src={hills}></img> */}
+
+      </div>
+
+  </ThemeProvider>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,56 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createTheme, ThemeProvider } from '@mui/material';
+
 import App from './App.jsx'
 import './main.css';
+
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 
+const theme = createTheme({
+  spacing: 16,
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#93B7BE',
+      contrastText: '#1C2321'
+    },
+    secondary: {
+      main: '#C1666B',
+      contrastText: '#f5f5f5'
+    },
+    tertiary: {
+      main: "#285943"
+    },
+    eerieBlack: {
+      main: "#1C2321"
+    }
+  },
+  components: {
+    MuiSvgIcon: {
+      styleOverrides: {
+        root: {
+          fontSize : "70px"
+        }
+      }
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: {
+          textTransform: 'none',
+        }
+      }
+    }
+  }
+})
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
